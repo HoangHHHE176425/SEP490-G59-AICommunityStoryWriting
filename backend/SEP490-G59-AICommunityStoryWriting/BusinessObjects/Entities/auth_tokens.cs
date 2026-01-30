7a362eb9ae76bad7eddbc7a2fa4b48d1aee7cabf
Hoanghhhe176425 <hoanghhhe176425@fpt.edu.vn>
2026-01-30 22:56:48 +0700

    Add EF Core scaffolded entities for story_platform database

---
+++ backend/SEP490-G59-AICommunityStoryWriting/BusinessObjects/Entities/auth_tokens.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObjects.Entities;
+
+public partial class auth_tokens
+{
+    public string id { get; set; } = null!;
+
+    public string user_id { get; set; } = null!;
+
+    public string refresh_token { get; set; } = null!;
+
+    public string? device_info { get; set; }
+
+    public DateTime expires_at { get; set; }
+
+    public DateTime? created_at { get; set; }
+
+    public virtual users user { get; set; } = null!;
+}
