72499a4ff9725e666576d2bc61c294742a966489
Manhquyen2003 <dinhmanhquyen0202@gmail.com>
2026-02-02 09:48:50 +0700

    create detail chapter screen

--- frontend/sep490-g59-aicommunitystorywriting/src/App.jsx
+++ frontend/sep490-g59-aicommunitystorywriting/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/Homepage';
 import { StoryDetail } from './pages/story-detail/StoryDetail';
 import { AdminPage } from './pages/admin/AdminPage';
+import { ChapterReader } from './pages/chapter-detail/ChapterReader';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/story" element={<StoryDetail />} />
         <Route path="/admin" element={<AdminPage />} />
+        <Route path="/chapter" element={<ChapterReader />} />
       </Routes>
     </BrowserRouter>
   );
