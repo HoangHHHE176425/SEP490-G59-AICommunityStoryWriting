c8d5dd326b2588d2abbcf545b3a28bd4a290f1c9
Hoanghhhe176425 <hoanghhhe176425@fpt.edu.vn>
2026-02-02 19:20:23 +0700

    add repository implementations

---
+++ backend/SEP490-G59-AICommunityStoryWriting/Repositories/Implementations/StoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using BusinessObjects.Entities;
+using DataAccessObjects.DAOs;
+
+namespace Repositories
+{
+    public class StoryRepository : IStoryRepository
+    {
+        public IQueryable<story> GetAll()
+            => StoryDAO.GetAll();
+
+        public story? GetById(int id)
+            => StoryDAO.GetById(id);
+
+        public void Add(story story)
+            => StoryDAO.Add(story);
+
+        public void Update(story story)
+            => StoryDAO.Update(story);
+
+        public void Delete(int id)
+            => StoryDAO.Delete(id);
+    }
+}
