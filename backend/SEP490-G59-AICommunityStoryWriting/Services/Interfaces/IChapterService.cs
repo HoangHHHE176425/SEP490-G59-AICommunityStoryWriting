1edfe67abc3aa36f8b37059cc2e1650e4612a18b
Hoanghhhe176425 <hoanghhhe176425@fpt.edu.vn>
2026-02-03 17:11:06 +0700

    implement chapter APIs to support story reading flow

---
+++ backend/SEP490-G59-AICommunityStoryWriting/Services/Interfaces/IChapterService.cs
@@ -0,0 +1,16 @@
+﻿using Services.DTOs.Chapters;
+using Services.DTOs.Stories;
+
+public interface IChapterService
+{
+    ChapterResponseDto Create(CreateChapterRequestDto request);
+    PagedResultDto<ChapterListItemDto> GetAll(ChapterQueryDto query);
+    ChapterResponseDto? GetById(int id);
+    IEnumerable<ChapterListItemDto> GetByStoryId(int storyId);
+    ChapterResponseDto? GetByStoryIdAndOrderIndex(int storyId, int orderIndex);
+    bool Update(int id, UpdateChapterRequestDto request);
+    bool Delete(int id);
+    bool Publish(int id);
+    bool Unpublish(int id);
+    bool Reorder(int id, int newOrderIndex);
+}
