09c84e276c32f880c7bc8beacb737692c98fe170
Hoanghhhe176425 <hoanghhhe176425@fpt.edu.vn>
2026-02-03 17:11:06 +0700

    implement chapter APIs to support story reading flow

---
+++ backend/SEP490-G59-AICommunityStoryWriting/DataAccessObjects/DAOs/ChapterDAO.cs
@@ -0,0 +1,52 @@
+﻿using BusinessObjects.DataAccessObjects.Context;
+using BusinessObjects.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessObjects.DAOs
+{
+    public class ChapterDAO
+    {
+        public static IQueryable<chapter> GetAll()
+        {
+            var context = new StoryPlatformDbContext();
+            return context.chapters.AsNoTracking();
+        }
+
+        public static chapter? GetById(int id)
+        {
+            using var context = new StoryPlatformDbContext();
+            return context.chapters.FirstOrDefault(c => c.id == id);
+        }
+
+        public static chapter? GetByStoryIdAndOrderIndex(int storyId, int orderIndex)
+        {
+            using var context = new StoryPlatformDbContext();
+            return context.chapters.FirstOrDefault(c => c.story_id == storyId && c.order_index == orderIndex);
+        }
+
+        public static void Add(chapter chapter)
+        {
+            using var context = new StoryPlatformDbContext();
+            context.chapters.Add(chapter);
+            context.SaveChanges();
+        }
+
+        public static void Update(chapter chapter)
+        {
+            using var context = new StoryPlatformDbContext();
+            context.chapters.Update(chapter);
+            context.SaveChanges();
+        }
+
+        public static void Delete(int id)
+        {
+            using var context = new StoryPlatformDbContext();
+            var chapter = context.chapters.FirstOrDefault(c => c.id == id);
+            if (chapter != null)
+            {
+                context.chapters.Remove(chapter);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ backend/SEP490-G59-AICommunityStoryWriting/Repositories/Implementations/ChapterRepository.cs
@@ -0,0 +1,31 @@
+﻿using BusinessObjects.Entities;
+using DataAccessObjects.DAOs;
+
+namespace Repositories
+{
+    public class ChapterRepository : IChapterRepository
+    {
+        public IQueryable<chapter> GetAll()
+            => ChapterDAO.GetAll();
+
+        public chapter? GetById(int id)
+            => ChapterDAO.GetById(id);
+
+        public IEnumerable<chapter> GetByStoryId(int storyId)
+            => ChapterDAO.GetAll()
+                .Where(c => c.story_id == storyId)
+                .ToList();
+
+        public chapter? GetByStoryIdAndOrderIndex(int storyId, int orderIndex)
+            => ChapterDAO.GetByStoryIdAndOrderIndex(storyId, orderIndex);
+
+        public void Add(chapter chapter)
+            => ChapterDAO.Add(chapter);
+
+        public void Update(chapter chapter)
+            => ChapterDAO.Update(chapter);
+
+        public void Delete(int id)
+            => ChapterDAO.Delete(id);
+    }
+}
