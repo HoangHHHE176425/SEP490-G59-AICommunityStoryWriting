709d2698ebfe37948487452e78fcdbebdce120c6
Hoanghhhe176425 <hoanghhhe176425@fpt.edu.vn>
2026-02-03 17:10:45 +0700

    add category APIs and services for story classification

---
+++ backend/SEP490-G59-AICommunityStoryWriting/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using BusinessObjects.Entities;
+using Services.DTOs.Categories;
+
+namespace Services.Interfaces
+{
+    public interface ICategoryService
+    {
+        CategoryResponseDto Create(CreateCategoryRequestDto request);
+        IEnumerable<CategoryResponseDto> GetAll(bool includeInactive = false);
+        CategoryResponseDto? GetById(int id);
+        CategoryResponseDto? GetBySlug(string slug);
+        bool Update(int id, UpdateCategoryRequestDto request);
+        bool Delete(int id);
+        bool ToggleActive(int id);
+    }
+}
