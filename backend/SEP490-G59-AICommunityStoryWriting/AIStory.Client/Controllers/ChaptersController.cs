3f81cc2c8dcbda42d01173de8bb9ddf31ffdb3e4
Hoanghhhe176425 <hoanghhhe176425@fpt.edu.vn>
2026-02-03 17:11:53 +0700

    add lightweight client for API testing before FE integration

---
+++ backend/SEP490-G59-AICommunityStoryWriting/AIStory.Client/Controllers/ChaptersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AIStory.Client.Controllers
+{
+    public class ChaptersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ByStory(int? storyId)
+        {
+            if (!storyId.HasValue)
+            {
+                return RedirectToAction("Index");
+            }
+            ViewBag.StoryId = storyId.Value;
+            return View();
+        }
+
+        public IActionResult Read(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return RedirectToAction("Index", "Stories");
+            }
+            ViewBag.ChapterId = id.Value;
+            return View();
+        }
+    }
+}
