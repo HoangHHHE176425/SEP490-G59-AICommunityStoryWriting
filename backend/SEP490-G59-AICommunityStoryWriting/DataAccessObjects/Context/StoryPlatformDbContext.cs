32fda9024c989aca58a93244e51b8e3b371caaa4
Hoanghhhe176425 <hoanghhhe176425@fpt.edu.vn>
2026-02-09 22:02:18 +0700

    Restore and reapply category, chapter, story pagination and API changes after test revert

--- backend/SEP490-G59-AICommunityStoryWriting/DataAccessObjects/Context/StoryPlatformDbContext.cs
+++ backend/SEP490-G59-AICommunityStoryWriting/DataAccessObjects/Context/StoryPlatformDbContext.cs
@@ -106,7 +106,7 @@ public partial class StoryPlatformDbContext : DbContext
     {
         if (!optionsBuilder.IsConfigured)
         {
-            optionsBuilder.UseSqlServer("Server= TRUONG\\HIHITRUONGNE;uid=sa;password=123;database=story_platform_v6;Encrypt=True;TrustServerCertificate=True;");
+            optionsBuilder.UseSqlServer("Server=HOANGBOSUA\\SQLEXPRESS;uid=sa;password=a123;database=story_platform_v13;Encrypt=True;TrustServerCertificate=True;");
         }
     }
 
