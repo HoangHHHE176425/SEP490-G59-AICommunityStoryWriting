38f26d3bf8c972067707cc05b67fd91d30ae132b
Manhquyen2003 <dinhmanhquyen0202@gmail.com>
2026-02-11 12:43:51 +0700

    remove story type in category

--- backend/SEP490-G59-AICommunityStoryWriting/DataAccessObjects/Context/StoryPlatformDbContext.cs
+++ backend/SEP490-G59-AICommunityStoryWriting/DataAccessObjects/Context/StoryPlatformDbContext.cs
@@ -108,7 +108,7 @@ public partial class StoryPlatformDbContext : DbContext
     {
         if (!optionsBuilder.IsConfigured)
         {
-            optionsBuilder.UseSqlServer("Server= TRUONG\\HIHITRUONGNE;uid=sa;password=123;database=story_platform_v13;Encrypt=True;TrustServerCertificate=True;");
+            optionsBuilder.UseSqlServer("Server= localhost;uid=sa;password=admin;database=story_platform_v13;Encrypt=True;TrustServerCertificate=True;");
 
         }
     }
