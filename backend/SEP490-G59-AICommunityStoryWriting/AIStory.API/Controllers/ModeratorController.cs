8c0714a006b8498fdbbdc94e709e8c633df31b6b
Manhquyen2003 <dinhmanhquyen0202@gmail.com>
2026-03-05 22:25:11 +0700

    handle logic call api reject publish story - realtime

--- backend/SEP490-G59-AICommunityStoryWriting/AIStory.API/Controllers/ModeratorController.cs
+++ backend/SEP490-G59-AICommunityStoryWriting/AIStory.API/Controllers/ModeratorController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Repositories.Interfaces;
 using Services.DTOs.Moderation;
